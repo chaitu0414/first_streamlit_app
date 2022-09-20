7b4699083869b94f5af1e46aaff1a869f86d3587
Krishna Chilukuri <113998403+chaitu0414@users.noreply.github.com>
2022-09-20 19:13:41 +0530

    Update streamlit_app.py
    added new code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
-streamlit.title('My parents new Diner')
-streamlit.header('New')
-streamlit.text('Menu')
+streamlit.title('My parents new diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
